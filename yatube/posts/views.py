8d7d872426b9fdecce7a1b55efba8441591c3140
ArslanYadov <arsyy90@gmail.com>
2022-05-20 01:07:25 +0300

    Add profile page, post detail and paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
-from multiprocessing import context
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
-from .models import Post, Group
+from .models import Post, Group, User
+from django.db.models import Count
 
 
 POSTS_AMOUNT = 10
@@ -10,13 +10,17 @@ POSTS_AMOUNT = 10
 def index(request):
     paginator = (
         Paginator(
-            Post.objects.
-            select_related('author', 'group'),
+            Post.objects
+            .select_related('author', 'group'),
             POSTS_AMOUNT
         )
     )
     page_obj = paginator.get_page(request.GET.get('page'))
-    return render(request, 'posts/index.html', {'page_obj': page_obj,})
+    return render(
+        request,
+        'posts/index.html',
+        {'page_obj': page_obj,}
+    )
 
 
 def group_posts(request, slug):
@@ -34,5 +38,45 @@ def group_posts(request, slug):
         {
             'group': group,
             'page_obj': page_obj,
+        }
+    )
+
+
+def profile(request, username):
+    paginator = (
+        Paginator(
+            Post.objects.
+            select_related('author', 'group')
+            .filter(author__username=username),
+            POSTS_AMOUNT
+        )
+    )
+    page_obj = paginator.get_page(request.GET.get('page'))
+    user = User.objects.get(username=username)
+    amount = Post.objects.filter(author__username=username).count()
+    return render(
+        request,
+        'posts/profile.html',
+        {
+            'page_obj': page_obj,
+            'user': user,
+            'amount': amount,
+        }
+    )
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    amount = (
+        Post.objects
+        .filter(author__username=post.author)
+        .annotate(count=Count('author'))
+    )
+    return render(
+        request,
+        'posts/post_detail.html',
+        {
+            'post': post,
+            'amount': amount,
         }
     )
